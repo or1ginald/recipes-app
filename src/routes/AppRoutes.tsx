9ab48f5e35c1e60d9ae0080d16aaee15908c0367
Vorobev Ivan <superhd@icloud.com>
2022-03-16 22:52:14 +0300

    Added routes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import {Route, Routes } from 'react-router-dom';
 import {Home} from "./Home";
 import {Cuisine} from "./Cuisine";
+import {Searched} from "./Searched";
+import {Recipe} from "./Recipe";
 
 export const AppRoutes = () => {
     return (
         <Routes>
             <Route path={'/'} element={<Home/>}/>
             <Route path={'/cuisine/:type'} element={<Cuisine/>}/>
+            <Route path={'/searched/:search'} element={<Searched/>}/>
+            <Route path={'/recipe/:id'} element={<Recipe/>}/>
+
+
 
         </Routes>
     );
